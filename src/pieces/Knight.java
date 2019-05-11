b4619fc4c3bdaff79f957feecd3c5c4572aca3f6
Karel Hanák <xhanak34@vutbr.cz>
2019-05-11 15:08:52 +0200

    knight ADD valid movement

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -23,6 +23,21 @@ public class Knight implements Piece {
 
     @Override
     public boolean isValidMovement(Tile from, Tile to, Tile[][] tiles) {
-        return false;
+        int fromX = from.getCol();
+        int fromY = from.getRow();
+        int toX = to.getCol();
+        int toY = to.getRow();
+        int diffX = Math.abs(fromX - toX);
+        int diffY = Math.abs(fromY - toY);
+
+        if ((diffX == 1 && diffY == 2) || (diffX == 2 && diffY == 1)) {
+            if (to.isEmpty()) {
+                return true;
+            } else {
+                return (from.getPiece().getColor() != to.getPiece().getColor());
+            }
+        } else {
+            return false;
+        }
     }
 }
