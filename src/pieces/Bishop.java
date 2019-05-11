2ab557a000c3fcc345b574e71fd7e3d8997f4039
Karel Hanák <xhanak34@vutbr.cz>
2019-05-11 14:50:11 +0200

    bishop ADD valid movement

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -23,6 +23,66 @@ public class Bishop implements Piece {
 
     @Override
     public boolean isValidMovement(Tile from, Tile to, Tile[][] tiles) {
+        int fromX = from.getCol();
+        int fromY = from.getRow();
+        int toX = to.getCol();
+        int toY = to.getRow();
+        int diffX = Math.abs(fromX - toX);
+        int diffY = Math.abs(fromY - toY);
+
+        if ((diffX == diffY) && diffX != 0) { //bishop moves to a valid tile
+            if (fromX < toX && fromY < toY) { //moving down and right
+                for (int i = 1; i < diffX; i++) {
+                    Tile current = tiles[fromX+i][fromY+i];
+                    if (!current.isEmpty()) {
+                        return false;
+                    }
+                }
+                if (to.isEmpty()) {
+                    return true;
+                } else {
+                    return (from.getPiece().getColor() != to.getPiece().getColor());
+                }
+            } else if (fromX > toX && fromY < toY) { //moving down and left
+                for (int i = 1; i < diffX; i++) {
+                    Tile current = tiles[fromX-i][fromY+i];
+                    if (!current.isEmpty()) {
+                        return false;
+                    }
+                }
+                if (to.isEmpty()) {
+                    return true;
+                } else {
+                    return (from.getPiece().getColor() != to.getPiece().getColor());
+                }
+            } else if (fromX < toX && fromY > toY) { //moving up and right
+                for (int i = 1; i < diffX; i++) {
+                    Tile current = tiles[fromX+i][fromY-i];
+                    if (!current.isEmpty()) {
+                        return false;
+                    }
+                }
+                if (to.isEmpty()) {
+                    return true;
+                } else {
+                    return (from.getPiece().getColor() != to.getPiece().getColor());
+                }
+            } else if (fromX > toX && fromY > toY) { //moving up and left
+                for (int i = 1; i < diffX; i++) {
+                    Tile current = tiles[fromX-i][fromY-i];
+                    if (!current.isEmpty()){
+                        return false;
+                    }
+                }
+                if (to.isEmpty()) {
+                    return true;
+                } else {
+                    return (from.getPiece().getColor() != to.getPiece().getColor());
+                }
+            }
+        } else {
+            return false;
+        }
         return false;
     }
 }
